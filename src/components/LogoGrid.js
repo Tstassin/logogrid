273a4a627e13a018a5da6a1fc7e970e41dd47d72
thomas <thomas@MacBook-Pro.local>
2019-05-06 21:34:22 +0200

    With LogoGrid Panel basically working with flex directive

--- src/components/LogoGrid.js
+++ src/components/LogoGrid.js
@@ -3,7 +3,13 @@ import React from 'react'
 class LogoGrid extends React.Component {
     render() {
         return (
-            <div>LogoGrid</div>
+            <div className="logogrid">
+                {this.props.imageList.map(
+                    (image, index) => {
+                        return <img key={"logo-"+index} src={image.url} />
+                    }
+                )}
+            </div>
         )
     }
 }

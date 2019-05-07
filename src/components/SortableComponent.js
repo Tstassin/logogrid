5ad70fd73cc84ba00d8430b43ee67f0c15f913ef
thomas <thomas@MacBook-Pro.local>
2019-05-07 17:48:19 +0200

    image previews now sortable, but awful ui

--- src/components/SortableComponent.js
+++ src/components/SortableComponent.js
@@ -1,25 +1,31 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 
-const SortableItem = SortableElement(({value}) => <li>{value.name}</li>);
+const SortableItem = SortableElement(
+    ({ value, index }) => 
+        <li className="img" key={"image-" + index}>
+            <img src={value.preview} onClick={() => { this.props.deleteImage(index) }}
+                alt={value.name + " - " + value.size + " bytes"} />
+        </li>
+)
 
-const SortableList = SortableContainer(({items}) => {
-  return (
-    <ul>
-      {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} />
-      ))}
-    </ul>
-  )
+const SortableList = SortableContainer(({ items }) => {
+    return (
+        <ul>
+            {items.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value} />
+            ))}
+        </ul>
+    )
 })
 
 class SortableComponent extends Component {
-      
-  render() {
-    console.log(this.props.imageList)
-    return <SortableList items={this.props.imageList} onSortEnd={this.props.onSortEnd} />;
-  }
+
+    render() {
+        console.log(this.props.imageList)
+        return <SortableList items={this.props.imageList} onSortEnd={this.props.onSortEnd} />;
+    }
 }
 
 export default SortableComponent

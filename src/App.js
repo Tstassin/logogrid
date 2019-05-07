85731f772085faceced9a557c3ca759344c9648b
thomas <thomas@MacBook-Pro.local>
2019-05-07 11:33:52 +0200

    First workable fullscreen dropzone

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import SplitLayout from './components/SplitLayout'
 import LogoGrid from './components/LogoGrid'
 import ImageList from './components/ImageList'
+import FullScreenDropZone from './components/FullScreenDropZone'
 import './App.css';
 
 function App() {
   return (
-    <SplitLayout>
-      <ImageList></ImageList>
-      <LogoGrid></LogoGrid>
-    </SplitLayout>
+    <FullScreenDropZone>
+      <SplitLayout>
+        <ImageList></ImageList>
+        <LogoGrid></LogoGrid>
+      </SplitLayout>
+    </FullScreenDropZone>
   )
 }
 

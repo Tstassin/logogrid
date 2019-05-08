5ca4a83e3e8b659045af9f2a3c080ecd9c7d1f79
thomas <thomas@MacBook-Pro.local>
2019-05-08 18:09:07 +0200

    Add new left sidebar

--- src/components/SplitLayout.js
+++ src/components/SplitLayout.js
@@ -6,8 +6,11 @@ class SplitLayout extends React.Component {
     render() {
         return (
             <div>
-                <div className="leftPanel">
+                <div className="topPanel">
                     {React.cloneElement(this.props.children[0], { imageList: this.props.imageList, deleteImage: this.props.deleteImage, onSortEnd: this.props.onSortEnd })}
+                </div>
+                <div className="leftPanel">
+                    FLEX
                 </div>
                 <div className="rightPanel">
                     {React.cloneElement(this.props.children[1], { imageList: this.props.imageList })}

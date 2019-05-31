e55c08b6f8051d63e2342dc27d39c43081a11a91
thomas <thomas@MacBook-Pro.local>
2019-05-31 14:02:32 +0200

    Adding visual cues of canvas sizing... half done

--- src/components/LogoGrid.js
+++ src/components/LogoGrid.js
@@ -3,15 +3,24 @@ import React from 'react'
 class LogoGrid extends React.Component {
     render() {
         return (
-            <div className="canvas" style={{ ...this.props.globals, background: "white" }}>
-                <div className="logogrid" style={{ ...this.props.properties, display: "flex" }}>
-                    {this.props.imageOperations.imageList.map(
-                        (image, index) => {
-                            return <img key={"logo-" + index} style={{ ...this.props.inputs }} src={image.preview} alt="" />
-                        }
-                    )}
+            <div className="logogrid">
+                <div className="canvasWrapper">
+                    <div class="showSize width">
+                        <span class="size">{this.props.globals.width}</span>
+                        <div class="showSize height">
+                            <span class="size">{this.props.globals.height}</span>
+                            <div className="canvas" style={{ ...this.props.properties, ...this.props.globals, display: "flex", background: "white" }}>
+                                {this.props.imageOperations.imageList.map(
+                                    (image, index) => {
+                                        return <img key={"logo-" + index} style={{ ...this.props.inputs }} src={image.preview} alt="" />
+                                    }
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         )
     }
 }

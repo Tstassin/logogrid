7700b06b043c00fdbbba56882d5a9a6ecd13a0d7
thomas <thomas@MacBook-Pro.local>
2019-06-01 12:43:59 +0200

    flexMenu Good !

--- src/components/FlexMenu.js
+++ src/components/FlexMenu.js
@@ -8,9 +8,9 @@ class FlexMenu extends React.Component {
     }
 
     export = () => {
-        html2canvas(document.getElementById("canvas"), {useCORS: true, }).then((image) => {
+        html2canvas(document.getElementById("canvas"), { useCORS: true, }).then((image) => {
             console.log(image)
-            this.setState({exportedImage: image})
+            this.setState({ exportedImage: image })
         })
     }
     render() {
@@ -21,7 +21,7 @@ class FlexMenu extends React.Component {
                         {this.props.globals.map((global, globalIndex) => (
                             <tr name={global.name} index={globalIndex} key={global.name}>
                                 <td>
-                                    <div className="globalName">{global.name}</div>
+                                    <div className="label globalName">{global.name}</div>
                                 </td>
                                 <td>
                                     <input globalindex={globalIndex} type={global.type} value={global.type === "number" ? parseInt(global.value) : global.value} onChange={this.props.switchGlobalValue} />
@@ -31,7 +31,7 @@ class FlexMenu extends React.Component {
                         {this.props.properties.map((property, propertyIndex) => (
                             <tr name={property.name} index={propertyIndex} key={property.name}>
                                 <td>
-                                    <div className="propertyName">{property.name}</div>
+                                    <div className="label propertyName">{property.name}</div>
                                 </td>
                                 <td>
                                     {property.values.map((value, valueIndex) => (
@@ -49,7 +49,7 @@ class FlexMenu extends React.Component {
                         {this.props.inputs.map((input, inputIndex) => (
                             <tr name={input.name} index={inputIndex} key={input.name}>
                                 <td>
-                                    <div className="inputName">{input.name}</div>
+                                    <div className="label inputName">{input.name}</div>
                                 </td>
                                 <td>
                                     <input inputindex={inputIndex} type={input.type} value={input.type === "number" ? parseInt(input.value) : input.value} onChange={this.props.switchValue} />
@@ -58,9 +58,11 @@ class FlexMenu extends React.Component {
                         ))}
                     </tbody>
                 </table>
-                <input type="button" onClick={() => this.export()} value="export as image" />
-                <div className="exportedImageContainer">
-                    <img src={this.state.exportedImage ? this.state.exportedImage.toDataURL() : "" } />
+                <div className="exportAsImage">
+                    <input type="button" onClick={() => this.export()} value="export as image" />
+                    <div className="exportedImageContainer">
+                        <img src={this.state.exportedImage ? this.state.exportedImage.toDataURL() : ""} />
+                    </div>
                 </div>
             </div>
         )
